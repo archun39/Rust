bda6177ff80ff25737ad3c34757b51428c91dae1
Seonghyun Kim <archun39@kookmin.ac.kr>
2024-11-26 21:47:09 +0900

    chapter2 : coding

---
+++ chapter2/guessing_game/src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new(); //UTF-8
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, //밑줄은 포괄적인 값 -> 모든 오류 무시
+        };
+
+        println!("You guessed: {}", guess);
+
+        //gurss와 secret_number 비교
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
