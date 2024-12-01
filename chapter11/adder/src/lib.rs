f2e1919bcd0bf4c4ee869cb18a8257af2f8fed43
Seonghyun Kim <archun39@kookmin.ac.kr>
2024-12-01 19:05:38 +0900

    chatper11 : assert

--- chapter11/adder/src/lib.rs
+++ chapter11/adder/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
